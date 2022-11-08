3150f089912d8300f5b598fceaae65fc362dcce9
Lucas Spizzica <nevergiveupltda@gmail.com>
2022-11-08 13:55:11 -0300

    Criação Projeto JAVA/Spring

---
+++ Back/turistae/turistae/src/test/java/com/facens/turistae/TuristaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.turistae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5a49e321907e514f658e4344a8d1a81c4adf8931
LcsSpizzica <lucas.spizzca@hotmail.com>
2023-05-09 20:21:02 -0300

    adicionando mais um recycler e organizações das telas

---
+++ app/src/main/java/com/app/turistae/TelaListaTurismos.java
@@ -0,0 +1,14 @@
+package com.app.turistae;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaListaTurismos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_lista_turismos);
+    }
+}
